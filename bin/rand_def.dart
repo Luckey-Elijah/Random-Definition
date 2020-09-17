ced8ebd8d5a62d640be1773cafb1669e246878ef
Elijah Luckey <luckeyelijah112@gmail.com>
2020-09-17 11:25:20 -0400

    init commit

---
+++ bin/rand_def.dart
@@ -0,0 +1,5 @@
+import 'package:rand_def/rand_def.dart' as rand_def;
+
+void main(List<String> arguments) {
+  print('Hello world: ${rand_def.calculate()}!');
+}
---
+++ test/rand_def_test.dart
@@ -0,0 +1,8 @@
+import 'package:rand_def/rand_def.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
