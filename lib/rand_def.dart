e00fdbbe9bee7156f217a2f7aae3b2d0165ac567
Elijah Luckey <luckeyelijah112@gmail.com>
2020-09-17 11:48:02 -0400

    adds [Definition] class

--- lib/rand_def.dart
+++ lib/rand_def.dart
@@ -1,3 +1,36 @@
-int calculate() {
-  return 6 * 7;
+import 'dart:convert';
+
+class Definiton {
+  final String word;
+  final String definition;
+  final String language;
+
+  const Definiton({
+    this.word,
+    this.definition,
+    this.language,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'word': word,
+      'definition': definition,
+      'language': language,
+    };
+  }
+
+  factory Definiton.fromMap(Map<String, dynamic> map) {
+    if (map == null) return null;
+
+    return Definiton(
+      word: map['word'],
+      definition: map['definition'],
+      language: map['language'],
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Definiton.fromJson(String source) =>
+      Definiton.fromMap(json.decode(source));
 }
